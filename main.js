d1f5d503981e13ff0dae47e739331a9ebb2667b7
mjjjj14 <142198793+mjjjj14@users.noreply.github.com>
2024-02-14 14:18:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-// // Dino Animation
+// Dino Animation
 
-// let animation1Complete = false;
-// let animation2Complete = false;
+let animation1Complete = false;
+let animation2Complete = false;
 
-// var div = document.getElementById('hiddenDiv');
+var div = document.getElementById('hiddenDiv');
 
-// document.querySelector('.dino1').addEventListener('animationend', () => {
-//     // Animation 1 is complete, start Animation 2
-//     document.querySelector('.dino2').style.animation = 'animateRight 2s steps(11)';
+document.querySelector('.dino1').addEventListener('animationend', () => {
+    // Animation 1 is complete, start Animation 2
+    document.querySelector('.dino2').style.animation = 'animateRight 2s steps(11)';
 
-//     animation1Complete = true;
+    animation1Complete = true;
 
-//     animateAndShow();
-// });
+    animateAndShow();
+});
 
-// document.querySelector('.dino2').addEventListener('animationend', () => {
+document.querySelector('.dino2').addEventListener('animationend', () => {
 
-//     animation2Complete = true;
-//     animateAndShow();
-// });
+    animation2Complete = true;
+    animateAndShow();
+});
 
 
-// function animateAndShow() {
+function animateAndShow() {
   
-//     if (animation1Complete && animation2Complete ) {
-//         document.getElementById('hiddenDiv').style.display = 'flex';
-//     }
+    if (animation1Complete && animation2Complete ) {
+        document.getElementById('hiddenDiv').style.display = 'flex';
+    }
 
-//     div.style.opacity = '0';
-//     setTimeout(function() {
-//         div.style.opacity = '1'; // Fade in the div
-//         // You can add more animation logic here if needed
-//       }, 100); // Adjust the time according to your animation duration
-// }
+    div.style.opacity = '0';
+    setTimeout(function() {
+        div.style.opacity = '1'; // Fade in the div
+        // You can add more animation logic here if needed
+      }, 100); // Adjust the time according to your animation duration
+}
 
 
 // Image Slider
