859b32762af5142d89b0ca7f3cab2d9afdc53e56
mjjjj14 <142198793+mjjjj14@users.noreply.github.com>
2024-02-14 17:45:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-var button = document.getElementsByClassName("link-button");
-
-
-function typeEffect(element, speed) {
-	var text = element.innerHTML;
-	element.innerHTML = "";
-	
-	var i = 0;
-	var timer = setInterval(function() {
-    if (i < text.length) {
-      element.append(text.charAt(i));
-      i++;
-    } else {
-      clearInterval(timer);
-      button.style.display = 'flex';
-    }
-  }, speed);
-}
-
-
-// application
-var speed = 75;
-var h1 = document.querySelector('h1');
-var p = document.querySelector('p');
-var delay = h1.innerHTML.length * speed + speed;
-
-// type affect to header
-typeEffect(h1, speed);
-
-
-// type affect to body
-setTimeout(function(){
-  p.style.display = "inline-block";
-  typeEffect(p, speed);
-}, delay);
 var button = document.getElementsByClassName("link-button")[0]; // Assuming there's only one element with the class "link-button"
 
 function typeEffect(element, speed, callback) {
